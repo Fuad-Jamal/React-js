83497c92f2f7a09456612006b9de9a1b6ba95cad
Fuad Jamal <jamalfuad34@gmail.com>
2025-03-12 22:35:31 +0200

    learning usestate

---
+++ that-trial/src/MyComponent.jsx
@@ -0,0 +1,31 @@
+import React,{useState} from 'react'
+
+export const MyComponent = () => {
+
+    const [name, setName] = useState()
+    const [age, setAge] = useState(0)
+    const [employee, setEmployee] = useState(false)
+
+    const updateName = () => {
+        setName('Bombo')
+    }
+    const incrementAge = () => {
+        setAge(age +1)
+    }
+    const toggleStatus = () => {
+        
+    }
+
+  return (
+    <div>
+        <p>Name: {name}</p>
+        <button onClick= {updateName}>Set Name</button>
+
+        <p>Age: {age}</p>
+        <button onClick= {incrementAge}>Increnent Age</button>
+
+        <p>Employee: {employee? 'kbx': 'wapi'}</p>
+        <button onClick= {toggleStatus}>Akazi</button>
+    </div>
+  );
+}
