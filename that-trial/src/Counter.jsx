35729eb928999d9e512f86f9cdb70d6ab1b520a1
Fuad Jamal <jamalfuad34@gmail.com>
2025-03-16 10:28:56 +0200

    adding a counter component

---
+++ that-trial/src/Counter.jsx
@@ -0,0 +1,21 @@
+import React, {useState} from 'react'
+
+export const Counter = () => {
+    const [count, setCount] = useState(0)
+
+    const increment = () => {
+        setCount(count + 1)
+    }
+
+    const decrement = () =>{
+        setCount(count - 1)
+    }
+
+    const reset = ()=> {
+        
+    }
+
+  return (
+    <div>Counter</div>
+  )
+}
