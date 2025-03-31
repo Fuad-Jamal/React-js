9431f1e4935439dfb76b61c233fe9884a359ed66
Fuad Jamal <jamalfuad34@gmail.com>
2025-03-31 09:34:45 +0200

    adding prettier to My component file

--- that-trial/src/MyComponent.jsx
+++ that-trial/src/MyComponent.jsx
@@ -1,31 +1,30 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
 export const MyComponent = () => {
+  const [name, setName] = useState();
+  const [age, setAge] = useState(0);
+  const [employee, setEmployee] = useState(false);
 
-    const [name, setName] = useState()
-    const [age, setAge] = useState(0)
-    const [employee, setEmployee] = useState(false)
-
-    const updateName = () => {
-        setName('Bombo')
-    }
-    const incrementAge = () => {
-        setAge(age +1)
-    }
-    const toggleStatus = () => {
-        setEmployee(!employee)
-    }
+  const updateName = () => {
+    setName("Bombo");
+  };
+  const incrementAge = () => {
+    setAge(age + 1);
+  };
+  const toggleStatus = () => {
+    setEmployee(!employee);
+  };
 
   return (
     <div>
-        <p>Name: {name}</p>
-        <button onClick= {updateName}>Set Name</button>
+      <p>Name: {name}</p>
+      <button onClick={updateName}>Set Name</button>
 
-        <p>Age: {age}</p>
-        <button onClick= {incrementAge}>Increnent Age</button>
+      <p>Age: {age}</p>
+      <button onClick={incrementAge}>Increnent Age</button>
 
-        <p>Employee: {employee? 'kbx': 'wapi'}</p>
-        <button onClick= {toggleStatus}>Akazi</button>
+      <p>Employee: {employee ? "kbx" : "wapi"}</p>
+      <button onClick={toggleStatus}>Akazi</button>
     </div>
   );
-}
+};
